fc835dccb53d53885b0869c2603900dff8e271c6
MatheusKristman <kristman058@gmail.com>
2023-04-27 00:50:16 -0300

    deleted commented code

--- src/components/SearchPlans/index.jsx
+++ src/components/SearchPlans/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Box, Stack, Typography, Button } from '@mui/material';
 import { PlansContext } from '../../contexts/Plans/PlansContext';
 import { useContext, useRef } from 'react';
@@ -40,111 +40,7 @@ function SearchPlans() {
         }
     };
 
-    // useEffect(() => {
-    //     const handleOpenAnimation = () => {
-    //         backgroundRef.current.style.animation =
-    //             'fadeInBG 0.4s ease forwards';
-    //         modalRef.current.style.animation = 'fadeIn 0.2s ease 0.2s forwards';
-    //     };
-
-    //     handleOpenAnimation();
-    // }, []);
-
     return (
-        // <Box
-        //     sx={{
-        //         position: 'absolute',
-        //         width: '900px',
-        //         height: '400px',
-        //         background: '#fff',
-        //         top: '15%',
-        //         display: 'flex',
-        //         flexDirection: 'row-reverse',
-        //         borderRadius: '15px',
-        //     }}
-        // >
-        //     <Box sx={{ position: 'relative' }}>
-        //         <img
-        //             src='./assets/images/SearchPlans-bg.png'
-        //             alt='imagem de complemento'
-        //             style={{ width: '100%', height: '100%' }}
-        //         />
-        //         <button
-        //             style={{
-        //                 width: '30px',
-        //                 height: '30px',
-        //                 borderRadius: '8px',
-        //                 border: 'none',
-        //                 background: '#fff',
-        //                 fontSize: '16px',
-        //                 top: '10%',
-        //                 position: 'absolute',
-        //                 right: '10%',
-        //                 cursor: 'pointer',
-        //             }}
-        //             onClick={() => setSearchPlans(!searchPlans)}
-        //         >
-        //             X
-        //         </button>
-        //     </Box>
-        //     <Stack
-        //         sx={{
-        //             width: '59%',
-        //             height: '100%',
-        //             borderRadius: '15px',
-        //             padding: '2%',
-        //             justifyContent: 'space-evenly',
-        //         }}
-        //     >
-        //         <Box>
-        //             <Typography variant='h5' fontWeight='bold'>
-        //                 Antes de Começar
-        //             </Typography>
-        //         </Box>
-        //         <Stack
-        //             sx={{
-        //                 width: '100%',
-        //                 height: '80%',
-        //                 justifyContent: 'center',
-        //                 gap: '5%',
-        //             }}
-        //         >
-        //             <Typography variant='h5' fontWeight='medium'>
-        //                 Qual cidade que você mora?
-        //             </Typography>
-        //             <States />
-        //             <Link
-        //                 to='/hireplan'
-        //                 style={{
-        //                     width: '30%',
-        //                     height: '15%',
-        //                     borderRadius: '10px',
-        //                     border: 'none',
-        //                     background: '#D40066',
-        //                     color: '#fff',
-        //                     fontSize: '16px',
-        //                     cursor: 'pointer',
-        //                 }}
-        //             >
-        //                 <button
-        //                     type='submit'
-        //                     style={{
-        //                         width: '100%',
-        //                         height: '100%',
-        //                         borderRadius: '10px',
-        //                         border: 'none',
-        //                         background: '#D40066',
-        //                         color: '#fff',
-        //                         fontSize: '16px',
-        //                         cursor: 'pointer',
-        //                     }}
-        //                 >
-        //                     Procurar
-        //                 </button>
-        //             </Link>
-        //         </Stack>
-        //     </Stack>
-        // </Box>
         <Box
             sx={{
                 width: '100%',
