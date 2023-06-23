acdf80dcda5abc709f1910125430b767b50fadaf
MatheusKristman <kristman058@gmail.com>
2023-06-23 14:35:01 -0300

    fixed how it works animation for iphone

--- src/pages/Home/components/HomeHowItWorks.jsx
+++ src/pages/Home/components/HomeHowItWorks.jsx
@@ -1,5 +1,5 @@
-import React, { useMemo } from 'react';
-import { motion } from 'framer-motion';
+import React, { useMemo } from "react";
+import { motion } from "framer-motion";
 
 const StepsCard = ({ tag, title, desc }) => {
   const stepAnimation = useMemo(() => ({
@@ -8,41 +8,41 @@ const StepsCard = ({ tag, title, desc }) => {
   }));
 
   return (
-    <motion.div variants={stepAnimation} className='how-it-works-card'>
-      <span className='how-it-works-tag'>{tag}</span>
-      <h4 className='how-it-works-card-title'>{title}</h4>
-      <p className='how-it-works-desc'>{desc}</p>
+    <motion.div variants={stepAnimation} className="how-it-works-card">
+      <span className="how-it-works-tag">{tag}</span>
+      <h4 className="how-it-works-card-title">{title}</h4>
+      <p className="how-it-works-desc">{desc}</p>
     </motion.div>
   );
 };
 
 const HomeHowItWorks = () => {
   return (
-    <div id='howItWorks' className='how-it-works-container'>
-      <div className='wrapper'>
-        <h2 className='how-it-works-title'>Seu plano em apenas 3 passos</h2>
+    <div id="howItWorks" className="how-it-works-container">
+      <div className="wrapper">
+        <h2 className="how-it-works-title">Seu plano em apenas 3 passos</h2>
 
         <motion.div
           transition={{ staggerChildren: 0.3 }}
-          initial='offscreen'
-          whileInView='onscreen'
-          viewport={{ once: true, amount: 1 }}
-          className='how-it-works-wrapper'
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true, amount: 0.5 }}
+          className="how-it-works-wrapper"
         >
           <StepsCard
-            tag={'1'}
-            title={'Escolha um serviço'}
-            desc={'Selecione sua cidade e encontre o melhor plano para você'}
+            tag={"1"}
+            title={"Escolha um serviço"}
+            desc={"Selecione sua cidade e encontre o melhor plano para você"}
           />
           <StepsCard
-            tag={'2'}
-            title={'Compare'}
-            desc={'Utilize os filtros para facilitar a sua busca'}
+            tag={"2"}
+            title={"Compare"}
+            desc={"Utilize os filtros para facilitar a sua busca"}
           />
           <StepsCard
-            tag={'3'}
-            title={'Aproveite'}
-            desc={'Selecione o plano que você gostou e entre em contato'}
+            tag={"3"}
+            title={"Aproveite"}
+            desc={"Selecione o plano que você gostou e entre em contato"}
           />
         </motion.div>
       </div>
